cc4c2253ab36ff9d1b73cf0830b20c44375678ba
Aleksandr Liublinskii <shx7@users.noreply.github.com>
2021-06-18 15:23:37 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+
+//version 1
 import PackageDescription
 
 let package = Package(
